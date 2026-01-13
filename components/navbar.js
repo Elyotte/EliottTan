d03d39f69c487e81af28e1331daadf5836b0e56b
Eliott <tan.eliott04@gmail.com>
2026-01-13 07:58:59 +0100

    fixing component stickyness

--- components/navbar.js
+++ components/navbar.js
@@ -2,10 +2,16 @@ class NavBar extends HTMLElement {
   connectedCallback() {
     const isProjectPage = window.location.pathname.includes("/projects/")
 
+    this.style.position = 'sticky';
+    this.style.top = '0';
+    this.style.zIndex = '1000';
+    this.style.display = 'block';
+    this.style.width = '100%';
+    
     const base = isProjectPage ? "../index.html" : "index.html"
 
     this.innerHTML = `
-      <nav>
+      <nav class="nav">
         <a href="${base}#hero">Home</a>
         <a href="${base}#projects">Projects</a>
         <a href="${base}#footer">Contact</a>
