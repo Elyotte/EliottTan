f413307395e8bc57bd209ed33c1e5f93a6116ebb
Eliott <tan.eliott04@gmail.com>
2026-01-17 09:04:24 +0100

    removing inherintance

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,8 @@
-class NavBar extends MultipageComponent {
+class NavBar extends HTMLElement {
   connectedCallback() {
+    const isProjectPage = window.location.pathname.includes("/projects/");
+    const pathToIndex = isProjectPage ? "../index.html" : "index.html";  
+
     this.style.position = 'sticky';
     this.style.top = '0';
     this.style.zIndex = '1000';
